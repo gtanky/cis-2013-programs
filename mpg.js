0001f8a8a881520c73c84e4f2b3d9156a13248d8
gtanky <89743429+gtanky@users.noreply.github.com>
2021-09-17 17:12:07 -0500

    Add files via upload

---
+++ mpg.js
@@ -0,0 +1,9 @@
+start
+     declare floatMiles, floatGallons, floatMpg;
+     var floatMiles = prompt("Enter miles driven");
+     miles = parseFloat(miles);
+     var floatGallons = prompt("Enter gallons of gas used");
+     gallons = parseFloat(gallons);
+     var floatMpg = (miles/gallons);
+     alert("Miles per gallon = " + floatMpg);
+end
