612d44a536b4b344aafd82c0fc9cac8a3541621a
gtanky <89743429+gtanky@users.noreply.github.com>
2021-11-15 14:10:33 -0600

    Update ArrayPracticeEC.js

--- ArrayPracticeEC.js
+++ ArrayPracticeEC.js
@@ -1,4 +1,5 @@
 //Task 1
+
 var arrayNames = new Array("John", "Robert", "Jimmy", "Bonzo");
 
 var arrayInstruments = [];
@@ -11,26 +12,26 @@ var stringMessage = "";
 
 var intCount;
 
-for (intCount=0; intCount<arrayNames.length; intCount++)
+for (intCount = 0; intCount < arrayNames.length; intCount++)
 {
-    stringMessage = stringMessage + arrayNames[intCount] + " plays " + arrayInstruments[intCount] + "\n";
+    stringMessage = stringMessage + arrayNames[intCount] + " plays " + arrayInstruments[intCount] + "\n" ;
 }
-
 
 alert(stringMessage);
 //task 2
 
 var stringTimesTable = "";
 
-for (var x=1; x<=12; x++)
+for (var x=1; x <= 12; x++)
 {
-    for (var y=1; y<=12; y++) 
-    {
+    for (var y=1; y<= 12; y++) {
         stringTimesTable = stringTimesTable + " " + x*y + " ";
-    }
-    stringTimesTable = stringTimesTable + "\n";
+    } stringTimesTable = stringTimesTable + "\n";
 }
-alert(stringTimesTable);
+
+alert(stringTimesTable)
+
+
 
 
 
@@ -47,21 +48,18 @@ for (var x=1; x<=12; x++)
     {
         arrayTimesTable[x][y] = x*y;
     }
-}
+} 
 
 for (var a=1; a<=12; a++)
-{
-    if (a%2===0)
-    {
-        for (var b=1; b<=12; b++)
+        {
+    if (a%2===0) {
+        for (var b=1;b<=12;b++)
         {
             stringEvenTimes = stringEvenTimes + " " + arrayTimesTable[a][b] + " ";
         }
-        stringEvenTimes += "\n";
-    }
+    } stringEvenTimes = stringEvenTimes + "\n";
 }
 alert(stringEvenTimes);
-
 
 
 
